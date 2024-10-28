408b5bfdf5d3d63668fbdca1e5567d604d093bff
Eduardo Farfus <farfus@unochapeco.edu.br>
2024-10-28 19:40:36 -0300

    Tela de cadastro

--- src/paginas/Cadastro.js
+++ src/paginas/Cadastro.js
@@ -1,59 +1,98 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
+import TopBar from '../components/topbar';
 
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState(null);
 
-const Post = () => {
-    const { postId } = useParams();
-    const [post, setListaPosts] = React.useState([]);
-    const [replies, setListaReplies] = React.useState([]);
-    React.useEffect(() => {
-        const res = axios.get("blog/api/v1/rest/post/"+postId);
-        res.then((query) => {
-            setListaPosts(query.data);
-            console.log(post.id);
-        });
-        const res2 = axios.get("blog/api/v1/rest/postreply/"+postId);
-        res2.then((query) => {
-            setListaReplies(query.data);
-            console.log(replies);
-        });
-        
-      },
-    []);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setError("As senhas não coincidem.");
+      return;
+    }
+
+    try {
+      const response = await axios.post("blog/api/v1/rest/register", {
+        username,
+        email,
+        password,
+      });
+
+      console.log('Usuário registrado:', response.data);
+      alert('Cadastro realizado com sucesso!');
+      // Redirecionar para outra página ou limpar o formulário, conforme necessário
+    } catch (err) {
+      console.error('Erro ao registrar:', err);
+      setError('Erro ao registrar o usuário.');
+    }
+  };
+
   return (
-    <div>
-        { post && (
-        <div className="row">
-            <h1>{post.title}</h1>
-            <h2>{post.post}</h2>
-            <h5>Post #{postId}, created {post.createdAt}, updated {post.updatedAt}</h5>
-            <div className="row">
-                 {replies.length > 0 && (<table className="table">
-                     <thead>
-                         <tr>
-                             <th scope="col">ID</th>
-                             <th scope="col">Resposta</th>
-                             <th scope="col">Criado</th>
-                         </tr>
-                     </thead>
-                     <tbody>
-                         {replies &&
-                             replies.map((reply, index) => (
-                                 <tr key={index}>
-                                     <th scope="row" key={reply.id}>{reply.id}</th>
-                                     <td key={reply.reply}>{reply.reply}</td>
-                                     <td key={reply.createdAt}>{reply.createdAt}</td>
-                                 </tr>
-                             ))}
-                     </tbody>
-                 </table>)}
-             </div>
+    <div style={{ maxWidth: '400px', margin: '0 auto', padding: '20px' }}>
+        <TopBar /> {/* Adicionando a TopBar */}
+
+      <h1>Cadastro</h1>
+      <form onSubmit={handleSubmit}>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+
+        <div style={{ marginBottom: '15px' }}>
+          <label htmlFor="username">Nome de Usuário:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            style={{ width: '100%', padding: '8px', marginTop: '5px' }}
+          />
         </div>
-        )}
-      {/* Conteúdo da página "Sobre" */}
+
+        <div style={{ marginBottom: '15px' }}>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            style={{ width: '100%', padding: '8px', marginTop: '5px' }}
+          />
+        </div>
+
+        <div style={{ marginBottom: '15px' }}>
+          <label htmlFor="password">Senha:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            style={{ width: '100%', padding: '8px', marginTop: '5px' }}
+          />
+        </div>
+
+        <div style={{ marginBottom: '15px' }}>
+          <label htmlFor="confirmPassword">Confirmar Senha:</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+            style={{ width: '100%', padding: '8px', marginTop: '5px', marginBottom:'15px'}}
+          />
+        </div>
+
+        <button type="submit" style={{ width: '100%', padding: '10px', fontSize: '16px', marginLeft:'10px'}}>Cadastrar</button>
+      </form>
     </div>
   );
 };
 
-export default Post;
+export default Register;
--- src/paginas/Login.js
+++ src/paginas/Login.js
@@ -1,25 +1,26 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import TopBar from "../components/topbar"; // Importando o componente TopBar
-
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Aqui você pode adicionar a lógica de autenticação
     console.log('Email:', email);
     console.log('Password:', password);
   };
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-                <TopBar /> {/* Adicionando a TopBar */}
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <TopBar /> {/* Adicionando a TopBar */}
 
       <form onSubmit={handleSubmit} style={{ width: '300px', textAlign: 'center' }}>
-        
         <h2>Login</h2>
+        
         <div style={{ marginBottom: '15px' }}>
           <label htmlFor="email">Email:</label>
           <input
@@ -31,6 +32,7 @@ const Login = () => {
             style={{ width: '100%', padding: '8px', marginTop: '5px' }}
           />
         </div>
+        
         <div style={{ marginBottom: '15px' }}>
           <label htmlFor="password">Senha:</label>
           <input
@@ -42,9 +44,12 @@ const Login = () => {
             style={{ width: '100%', padding: '8px', marginTop: '5px' }}
           />
         </div>
-        <button type="submit" style={{ width: '100%', padding: '10px', fontSize: '16px' }}>Entrar</button>
-        <button type="button" style={{ width: '100%', padding: '10px', fontSize: '16px' }} onClick={() => alert('Ir para a página de cadastro')}>Cadastrar</button>
-        </form>
+
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', marginTop:'30px'}}>
+          <button type="submit" style={{ padding: '10px', fontSize: '16px', marginLeft: "25px" }}>Entrar</button>
+          <button type="button" style={{ padding: '10px', fontSize: '16px', marginLeft: "25px" }} onClick={() => navigate('/cadastro')}>Cadastrar</button>
+        </div>
+      </form>
     </div>
   );
 };
